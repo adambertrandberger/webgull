c949fdc60342ec7e428908a621095ba374dc7c0a
Adam Bertrand Berger <bergerab@uwm.edu>
2020-03-06 15:53:36 -0600

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ cwd = pathlib.Path(__file__).parent
 readme = (cwd / 'README.md').read_text()
 
 setup(name='webgull',
-      version='1.0.0',
+      version='1.1.0',
       license='GPL3',
       long_description=readme,
       long_description_content_type='text/markdown',
